60af475454d901b97af3069f1e3c6163e6a50ec3
Oliver Heamon <oliverheamon@gmail.com>
2023-01-31 12:49:55 +0000

    module

---
+++ Code/GunelineMk2Module.cs
@@ -0,0 +1,18 @@
+﻿using Celeste.Mod;
+
+namespace GunelineMk2.Code
+{
+    public class GunelineMk2Module : EverestModule
+    {
+        public static GunelineMk2Module Instance { get; private set; }
+
+        public override void Load()
+        {
+            Instance = this;
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
