b696c76ae3600c680a05bf5e20c06c56c9ec9515
Oliver Heamon <oliverheamon@gmail.com>
2023-01-31 12:59:13 +0000

    rename

---
+++ Code/GunelineModule.cs
@@ -0,0 +1,23 @@
+﻿using Celeste.Mod;
+using System;
+
+namespace GunelineMk2.Code
+{
+    public class GunelineModule : EverestModule
+    {
+        public static GunelineModule Instance { get; private set; }
+
+        public static GunelineSettings Settings => (GunelineSettings)Instance._Settings;
+
+        public override Type SettingsType => typeof(GunelineSettings);
+
+        public override void Load()
+        {
+            Instance = this;
+        }
+
+        public override void Unload()
+        {
+        }
+    }
+}
---
+++ Code/GunelineSettings.cs
@@ -0,0 +1,11 @@
+﻿using Celeste.Mod;
+using Microsoft.Xna.Framework.Input;
+
+namespace GunelineMk2.Code
+{
+    public class GunelineSettings : EverestModuleSettings
+    {
+        [DefaultButtonBinding(Buttons.Y, Keys.S)]
+        public ButtonBinding ShootButton { get; set; }
+    }
+}
